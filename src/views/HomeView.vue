1a75a31d05d3dbdd43e974e4d773dd887fbb20f9
huangcheng <936013542@qq.com>
2023-01-09 11:39:11 +0800

    222

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <span style="color: red">husky_eslint</span>
+    <span style="color: red">husky_eslint</span>，
   </div>
 </template>
 
